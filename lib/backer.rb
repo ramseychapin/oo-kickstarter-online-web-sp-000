fac581d89651506dd3dad1140f190df888fee83f
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-04 01:33:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,8 +3,8 @@ class Backer
   attr_accessor :backend_projects
   attr_reader :title
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
     @backend_projects = []
   end
 
